aba3c9cde1e212f76ba431502d04f8bc8d614d09
twilightgod <bychance@gmail.com>
2016-07-21 23:39:50 -0700

    bad one....need to handle all

--- app/src/main/java/com/rexpress/xposed/androidbrokenscreensaver/AndroidBrokenScreenSaverModule.java
+++ app/src/main/java/com/rexpress/xposed/androidbrokenscreensaver/AndroidBrokenScreenSaverModule.java
@@ -27,25 +27,62 @@ public class AndroidBrokenScreenSaverModule implements IXposedHookLoadPackage {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     MotionEvent event = (MotionEvent) param.args[0];
-                    //XLog("dispatchTouchEvent hooked: " + event.toString());
+                    int pointerIndex = event.getActionIndex();
+                    int pointerId = event.getPointerId(pointerIndex);
 
-                     float X = event.getX();
-                     float Y = event.getY();
+                    float X;
+                    float Y;
 
-                     if (Math.abs(Y - 259.8) < 1e-1 || Math.abs(Y - 211.8) < 1e-1 || Math.abs(Y - 70) < 1e-1) {
-                         param.setResult(true);
+                    // debug
+                    for (int i = 0; i < event.getPointerCount(); ++i) {
+                        X = event.getX(i);
+                        Y = event.getY(i);
 
-                         // write log
-                         String msg = "Throw away " + event.toString();
-                         XLog(msg);
+                        if (IsBadTouch(X, Y)) {
+                            String msg = "[debugtouch] " + event.toString();
+                            XLog(msg);
+                            break;
+                        }
+                    }
+                    MotionEvent.ob
+                    switch (event.getActionMasked()) {
+                        // Stop bad touch from begining...
+                        case MotionEvent.ACTION_DOWN:
+                        case MotionEvent.ACTION_POINTER_DOWN:
+                            // Get the triggering touch
+                            X = event.getX(pointerIndex);
+                            Y = event.getY(pointerIndex);
 
+                            // Skip this event, since it's triggered by a bad touch
+                            if (IsBadTouch(X, Y)) {
+                                param.setResult(true);
+                            }
+                            break;
+
+                        // We can safely skip move event since down events are been taken care of
+                        case MotionEvent.ACTION_MOVE:
+                        case MotionEvent.ACTION_UP:
+                        case MotionEvent.ACTION_POINTER_UP:
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    if (param.getResult() != null && (Boolean)param.getResult()) {
+                        // write log
+                        String msg = "Throw away " + event.toString();
+                        XLog(msg);
+                    }
+
+
+                         /*
                          Object currentObj = param.thisObject;
                          Activity currentActivity;
                          if (currentObj instanceof Activity) {
                              currentActivity = (Activity) currentObj;
-                             Toast.makeText(currentActivity.getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                         }
-                     }
+                             //Toast.makeText(currentActivity.getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                         }*/
                 }
             });
 
@@ -53,5 +90,9 @@ public class AndroidBrokenScreenSaverModule implements IXposedHookLoadPackage {
             XLog(E.toString());
         }
 
+    }
+
+    private boolean IsBadTouch(float X, float Y) {
+        return Math.abs(Y - 259.8) < 1e-1 || Math.abs(Y - 211.8) < 1e-1 || Math.abs(Y - 70) < 1e-1;
     }
 }
