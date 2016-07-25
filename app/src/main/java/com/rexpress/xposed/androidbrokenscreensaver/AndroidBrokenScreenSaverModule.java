249e1cd993476e9eb68727a5ea27d7a2178ae669
twilightgod <bychance@gmail.com>
2016-07-24 22:28:05 -0700

    single touch seems good now, but multi touch, especially move doesn't work

--- app/src/main/java/com/rexpress/xposed/androidbrokenscreensaver/AndroidBrokenScreenSaverModule.java
+++ app/src/main/java/com/rexpress/xposed/androidbrokenscreensaver/AndroidBrokenScreenSaverModule.java
@@ -27,62 +27,196 @@ public class AndroidBrokenScreenSaverModule implements IXposedHookLoadPackage {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     MotionEvent event = (MotionEvent) param.args[0];
+                    int action = event.getActionMasked();
                     int pointerIndex = event.getActionIndex();
                     int pointerId = event.getPointerId(pointerIndex);
+
 
                     float X;
                     float Y;
 
-                    // debug
-                    for (int i = 0; i < event.getPointerCount(); ++i) {
-                        X = event.getX(i);
-                        Y = event.getY(i);
+                    int toolType;
 
-                        if (IsBadTouch(X, Y)) {
-                            String msg = "[debugtouch] " + event.toString();
-                            XLog(msg);
-                            break;
+                    int j;
+
+                    // Get triggering touch
+                    X = event.getX(pointerIndex);
+                    Y = event.getY(pointerIndex);
+                    toolType = event.getToolType(pointerIndex);
+
+                    // Triggered by bad touch
+                    if (IsBadTouch(X, Y, toolType) && (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_POINTER_DOWN || action == MotionEvent.ACTION_POINTER_UP)) {
+                        XLog("Triggered by bad touch, ignore. " + event.toString());
+                        param.setResult(true);
+                        return;
+                    }
+
+                    // Multi-touch, triggered by normal touch
+                    if (action == MotionEvent.ACTION_POINTER_DOWN || action == MotionEvent.ACTION_POINTER_UP) {
+                        boolean[] bad = new boolean[event.getPointerCount()];
+                        int badCount = 0;
+
+                        for (int i = 0; i < event.getPointerCount(); ++i) {
+                            X = event.getX(i);
+                            Y = event.getY(i);
+                            toolType = event.getToolType(i);
+                            if (IsBadTouch(X, Y, toolType)) {
+                                bad[i] = true;
+                                badCount++;
+                            }
+                            else {
+                                bad[i] = false;
+                            }
+                        }
+
+                        // No bad touch, skip
+                        if (badCount == 0) {
+                            return;
+                        }
+
+                        // Filter out bad touches
+                        else {
+                            int goodCount = event.getPointerCount() - badCount;
+
+                            //getPointerProperties(int pointerIndex, MotionEvent.PointerProperties outPointerProperties)
+                            //Populates a MotionEvent.PointerProperties object with pointer properties for the specified pointer index.
+                            MotionEvent.PointerProperties[] pros = new MotionEvent.PointerProperties[goodCount];
+                            j = 0;
+                            for (int i = 0; i < event.getPointerCount(); ++i) {
+                                if (!bad[i]) {
+                                    pros[j] = new MotionEvent.PointerProperties();
+                                    event.getPointerProperties(i, pros[j]);
+                                    j++;
+                                }
+                            }
+
+                            MotionEvent.PointerCoords[] coods = new MotionEvent.PointerCoords[goodCount];
+                            j = 0;
+                            for (int i = 0; i < event.getPointerCount(); ++i) {
+                                if (!bad[i]) {
+                                    coods[j] = new MotionEvent.PointerCoords();
+                                    event.getPointerCoords(i, coods[j]);
+                                    j++;
+                                }
+                            }
+
+                            int newAction = action;
+
+                            // Only one touch and it's good, translate to single touch
+                            if (goodCount == 1) {
+                                newAction = action == MotionEvent.ACTION_POINTER_DOWN ? MotionEvent.ACTION_DOWN : MotionEvent.ACTION_UP;
+                            }
+                            // The main touch is good, action type does't change
+                            else {
+
+                            }
+
+
+                            MotionEvent newEvent = MotionEvent.obtain(
+                                    event.getDownTime(),
+                                    event.getEventTime(),
+                                    newAction,
+                                    goodCount,
+                                    pros,
+                                    coods,
+                                    event.getMetaState(),
+                                    event.getButtonState(),
+                                    event.getXPrecision(),
+                                    event.getYPrecision(),
+                                    event.getDeviceId(),
+                                    event.getEdgeFlags(),
+                                    event.getSource(),
+                                    event.getSource()
+                            );
+
+                            XLog("Filter up/down.");
+                            XLog(event.toString());
+                            XLog(newEvent.toString());
+                            param.args[0] = newEvent;
+                            return;
                         }
                     }
-                    MotionEvent.ob
-                    switch (event.getActionMasked()) {
-                        // Stop bad touch from begining...
-                        case MotionEvent.ACTION_DOWN:
-                        case MotionEvent.ACTION_POINTER_DOWN:
-                            // Get the triggering touch
-                            X = event.getX(pointerIndex);
-                            Y = event.getY(pointerIndex);
 
-                            // Skip this event, since it's triggered by a bad touch
-                            if (IsBadTouch(X, Y)) {
-                                param.setResult(true);
+                    // Move
+                    if (action == MotionEvent.ACTION_MOVE) {
+                        boolean[] bad = new boolean[event.getPointerCount()];
+                        int badCount = 0;
+
+                        for (int i = 0; i < event.getPointerCount(); ++i) {
+                            X = event.getX(i);
+                            Y = event.getY(i);
+                            toolType = event.getToolType(i);
+                            if (IsBadTouch(X, Y, toolType)) {
+                                bad[i] = true;
+                                badCount++;
                             }
-                            break;
+                            else {
+                                bad[i] = false;
+                            }
+                        }
 
-                        // We can safely skip move event since down events are been taken care of
-                        case MotionEvent.ACTION_MOVE:
-                        case MotionEvent.ACTION_UP:
-                        case MotionEvent.ACTION_POINTER_UP:
-                            break;
+                        // No bad touch, skip
+                        if (badCount == 0) {
+                            return;
+                        }
+                        // Only bad touch
+                        else if (badCount == event.getPointerCount()) {
+                            XLog("Move only contains bad touch, ignore. " + event.toString());
+                            param.setResult(true);
+                            return;
+                        }
+                        // Filter out bad touches
+                        else {
+                            int goodCount = event.getPointerCount() - badCount;
 
-                        default:
-                            break;
+                            //getPointerProperties(int pointerIndex, MotionEvent.PointerProperties outPointerProperties)
+                            //Populates a MotionEvent.PointerProperties object with pointer properties for the specified pointer index.
+                            MotionEvent.PointerProperties[] pros = new MotionEvent.PointerProperties[goodCount];
+                            j = 0;
+                            for (int i = 0; i < event.getPointerCount(); ++i) {
+                                if (!bad[i]) {
+                                    pros[j] = new MotionEvent.PointerProperties();
+                                    event.getPointerProperties(i, pros[j]);
+                                    j++;
+                                }
+                            }
+
+                            MotionEvent.PointerCoords[] coods = new MotionEvent.PointerCoords[goodCount];
+                            j = 0;
+                            for (int i = 0; i < event.getPointerCount(); ++i) {
+                                if (!bad[i]) {
+                                    coods[j] = new MotionEvent.PointerCoords();
+                                    event.getPointerCoords(i, coods[j]);
+                                    j++;
+                                }
+                            }
+
+                            MotionEvent newEvent = MotionEvent.obtain(
+                                    event.getDownTime(),
+                                    event.getEventTime(),
+                                    action,
+                                    goodCount,
+                                    pros,
+                                    coods,
+                                    event.getMetaState(),
+                                    event.getButtonState(),
+                                    event.getXPrecision(),
+                                    event.getYPrecision(),
+                                    event.getDeviceId(),
+                                    event.getEdgeFlags(),
+                                    event.getSource(),
+                                    event.getSource()
+                            );
+
+                            XLog("Filter move.");
+                            XLog(event.toString());
+                            XLog(newEvent.toString());
+                            param.args[0] = newEvent;
+                            return;
+                        }
                     }
 
-                    if (param.getResult() != null && (Boolean)param.getResult()) {
-                        // write log
-                        String msg = "Throw away " + event.toString();
-                        XLog(msg);
-                    }
-
-
-                         /*
-                         Object currentObj = param.thisObject;
-                         Activity currentActivity;
-                         if (currentObj instanceof Activity) {
-                             currentActivity = (Activity) currentObj;
-                             //Toast.makeText(currentActivity.getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                         }*/
+                    XLog("Normal touch: " + event.toString());
                 }
             });
 
@@ -92,7 +226,7 @@ public class AndroidBrokenScreenSaverModule implements IXposedHookLoadPackage {
 
     }
 
-    private boolean IsBadTouch(float X, float Y) {
-        return Math.abs(Y - 259.8) < 1e-1 || Math.abs(Y - 211.8) < 1e-1 || Math.abs(Y - 70) < 1e-1;
+    private boolean IsBadTouch(float X, float Y, int toolType) {
+        return toolType == MotionEvent.TOOL_TYPE_UNKNOWN || Math.abs(Y - 259.8) < 1e-1 || Math.abs(Y - 211.8) < 1e-1 || Math.abs(Y - 70) < 1e-1 || Math.abs(Y - 70) < 1e-1 || Math.abs(Y - 162.9) < 1e-1 || Math.abs(Y - 163.9) < 1e-1;
     }
 }
